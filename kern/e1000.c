f478440afe6bd527e629282bb43a63665779b1b7
185554981@qq.com <185554981@qq.com>
2020-04-23 17:22:46 +0800

    lab6 ex5

--- kern/e1000.c
+++ kern/e1000.c
@@ -2,11 +2,53 @@
 #include <kern/pmap.h>
 
 // LAB 6: Your driver code here
+#define TX_DESC_NUM 64
+#define PACKET_MAX_SIZE 1518
+
 volatile uint32_t *e1000_reg;
 
+struct tx_desc
+{
+	uint64_t addr;
+	uint16_t length;
+	uint8_t cso;
+	uint8_t cmd;
+	uint8_t status;
+	uint8_t css;
+	uint16_t special;
+};
+
+__attribute__((__aligned__(sizeof(struct tx_desc))))
+struct tx_desc  tx_ring[TX_DESC_NUM];
+
+char packet_buffer[TX_DESC_NUM][PACKET_MAX_SIZE];
+
+//按照手册声明数据结构来表示更好
+static void e1000_transmit_init(){
+	int i;
+	for(i=0;i<TX_DESC_NUM;i++){
+		tx_ring[i].addr=PADDR(&packet_buffer[i][0]);
+		tx_ring[i].length=PACKET_MAX_SIZE;
+		tx_ring[i].cmd=1<<3;
+		// tx_ring[i].cso=0;
+		// tx_ring[i].status&=0x0f;
+	}
+	e1000_reg[E1000_TDBAL/4]=PADDR(&tx_ring[0]);
+	e1000_reg[E1000_TDBAH/4]=0;
+	//
+	e1000_reg[E1000_TDLEN/4]=TX_DESC_NUM<<7;
+	e1000_reg[E1000_TDH/4]=0;
+	//index not byte offset
+	e1000_reg[E1000_TDT/4]=0;
+	e1000_reg[E1000_TCTL/4]=E1000_TCTL_EN|E1000_TCTL_PSP|(0x10<<4)|(0x40<<12);
+	e1000_reg[E1000_TIPG/4]=10|(4<<10)|(6<<20);
+}
+
 int pci_e1000_attach(struct pci_func *pcif){
-    pci_func_enable(pcif);
+	pci_func_enable(pcif);
     e1000_reg=mmio_map_region(pcif->reg_base[0],pcif->reg_size[0]);
     assert(e1000_reg[E1000_STATUS/4]==0x80080783);
-    return 1;
+	//transmit init
+	e1000_transmit_init();
+	return 1;
 }
--- kern/e1000.h
+++ kern/e1000.h
@@ -7,6 +7,20 @@
 // #define DEVICE_ID_82540EM 0x100E
 #define E1000_VENDOR_ID_82540EM 0x8086
 #define E1000_DEV_ID_82540EM 0x100E
+#define E1000_TCTL     0x00400  /* TX Control - RW */
+#define E1000_TIPG     0x00410  /* TX Inter-packet gap -RW */
+#define E1000_TDBAL    0x03800  /* TX Descriptor Base Address Low - RW */
+#define E1000_TDBAH    0x03804  /* TX Descriptor Base Address High - RW */
+#define E1000_TDLEN    0x03808  /* TX Descriptor Length - RW */
+#define E1000_TDH      0x03810  /* TX Descriptor Head - RW */
+#define E1000_TDT      0x03818  /* TX Descripotr Tail - RW */
+
+/* Transmit Control */
+#define E1000_TCTL_RST    0x00000001    /* software reset */
+#define E1000_TCTL_EN     0x00000002    /* enable tx */
+#define E1000_TCTL_PSP    0x00000008    /* pad short packets */
+#define E1000_TCTL_CT     0x00000ff0    /* collision threshold */
+#define E1000_TCTL_COLD   0x003ff000    /* collision distance */
 
 #define E1000_STATUS   0x00008  /* Device Status - RO */
 
