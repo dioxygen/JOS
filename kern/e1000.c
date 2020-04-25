d08960e97bc69bb9ad2f71530e1a3179892299ff
185554981@qq.com <185554981@qq.com>
2020-04-26 00:46:41 +0800

    lab6 ex10

--- kern/e1000.c
+++ kern/e1000.c
@@ -5,10 +5,15 @@
 
 // LAB 6: Your driver code here
 #define TX_DESC_NUM 64
+#define RX_DESC_NUM 128
 #define PACKET_MAX_SIZE 1518
+#define RX_BUFFER_SIZE 2048
 #define TDESC_STATUS_DD 1
 #define TDESC_CMD_EOP 1
 #define TDESC_CMD_RS 0x8
+//52:54:00:12:34:56, but this is written from lowest-order byte to highest-order byte
+#define E1000_MAC_LOW 0x12005452
+#define E1000_MAC_HIGH 0x5634
 volatile uint32_t *e1000_reg;
 
 struct tx_desc
@@ -22,16 +27,31 @@ struct tx_desc
 	uint16_t special;
 };
 
+/* Receive Descriptor */
+struct rx_desc {
+    uint64_t buffer_addr; /* Address of the descriptor's data buffer */
+    uint16_t length;     /* Length of data DMAed into data buffer */
+    uint16_t csum;       /* Packet checksum */
+    uint8_t status;      /* Descriptor status */
+    uint8_t errors;      /* Descriptor Errors */
+    uint16_t special;
+};
+
 __attribute__((__aligned__(sizeof(struct tx_desc))))
 struct tx_desc  tx_ring[TX_DESC_NUM];
 
-char packet_buffer[TX_DESC_NUM][PACKET_MAX_SIZE];
+char transmit_buffer[TX_DESC_NUM][PACKET_MAX_SIZE];
+
+__attribute__((__aligned__(sizeof(struct rx_desc))))
+struct rx_desc  rx_ring[RX_DESC_NUM];
+
+char receive_buffer[RX_DESC_NUM][RX_BUFFER_SIZE];
 
 //按照手册声明数据结构来表示更好
 static void e1000_transmit_init(){
 	int i;
 	for(i=0;i<TX_DESC_NUM;i++){
-		tx_ring[i].addr=PADDR(packet_buffer[i]);
+		tx_ring[i].addr=PADDR(transmit_buffer[i]);
 		tx_ring[i].status=TDESC_STATUS_DD;
 		// tx_ring[i].cso=0;
 		// tx_ring[i].status&=0x0f;
@@ -54,18 +74,40 @@ int transmit_packet(void *va,uint16_t length){
 		tx_ring[index].length=length;
 		tx_ring[index].cmd=TDESC_CMD_RS|TDESC_CMD_EOP;
 		tx_ring[index].status&=~TDESC_STATUS_DD;
-		memcpy(packet_buffer[index],va,length);
+		memcpy(transmit_buffer[index],va,length);
 		e1000_reg[E1000_TDT/4]=(index+1)%TX_DESC_NUM;
 		return 0;
 	}
 	return -E_TXRING_FULL;
 }
+
+static void e1000_receive_init(){
+	int i;
+	for(i=0;i<RX_DESC_NUM;i++){
+		rx_ring[i].buffer_addr=PADDR(receive_buffer[i]);
+		rx_ring[i].status=0;
+	}
+	//card MAC address
+	e1000_reg[E1000_RA/4]=E1000_MAC_LOW;
+	e1000_reg[(E1000_RA+4)/4]=E1000_MAC_HIGH|E1000_RAH_AV;
+	e1000_reg[E1000_MTA/4]=0;
+	//IMS:RXT,RXO,RXDMT,RXSEQ,LSC
+	e1000_reg[E1000_IMS/4]=E1000_IMS_LSC|E1000_IMS_RXSEQ|E1000_IMS_RXDMT0|E1000_IMS_RXO|E1000_IMS_RXT0;
+	e1000_reg[E1000_RDBAL/4]=PADDR(&rx_ring[0]);
+	e1000_reg[E1000_RDBAH/4]=0;
+	e1000_reg[E1000_RDLEN/4]=RX_DESC_NUM>>3<<7;
+	e1000_reg[E1000_RDH/4]=0;
+	e1000_reg[E1000_RDT/4]=RX_DESC_NUM-1;
+	e1000_reg[E1000_RCTL/4]=E1000_RCTL_EN|E1000_RCTL_BAM|E1000_RCTL_SZ_2048|E1000_RCTL_SECRC;
+}
+
 int pci_e1000_attach(struct pci_func *pcif){
 	pci_func_enable(pcif);
     e1000_reg=mmio_map_region(pcif->reg_base[0],pcif->reg_size[0]);
     assert(e1000_reg[E1000_STATUS/4]==0x80080783);
 	//transmit init
 	e1000_transmit_init();
+	e1000_receive_init();
 	// char *data="I'm here!";
 	// transmit_packet(data,0x2a);
 	return 1;
--- kern/e1000.h
+++ kern/e1000.h
@@ -7,6 +7,7 @@
 // #define DEVICE_ID_82540EM 0x100E
 #define E1000_VENDOR_ID_82540EM 0x8086
 #define E1000_DEV_ID_82540EM 0x100E
+#define E1000_RCTL     0x00100  /* RX Control - RW */
 #define E1000_TCTL     0x00400  /* TX Control - RW */
 #define E1000_TIPG     0x00410  /* TX Inter-packet gap -RW */
 #define E1000_TDBAL    0x03800  /* TX Descriptor Base Address Low - RW */
@@ -15,6 +16,16 @@
 #define E1000_TDH      0x03810  /* TX Descriptor Head - RW */
 #define E1000_TDT      0x03818  /* TX Descripotr Tail - RW */
 
+#define E1000_RDBAL    0x02800  /* RX Descriptor Base Address Low - RW */
+#define E1000_RDBAH    0x02804  /* RX Descriptor Base Address High - RW */
+#define E1000_RDLEN    0x02808  /* RX Descriptor Length - RW */
+#define E1000_RDH      0x02810  /* RX Descriptor Head - RW */
+#define E1000_RDT      0x02818  /* RX Descriptor Tail - RW */
+#define E1000_MTA      0x05200  /* Multicast Table Array - RW Array */
+#define E1000_RA       0x05400  /* Receive Address - RW Array */
+#define E1000_IMS      0x000D0  /* Interrupt Mask Set - RW */
+/* Receive Address */
+#define E1000_RAH_AV  0x80000000        /* Receive descriptor valid */
 /* Transmit Control */
 #define E1000_TCTL_RST    0x00000001    /* software reset */
 #define E1000_TCTL_EN     0x00000002    /* enable tx */
@@ -22,7 +33,41 @@
 #define E1000_TCTL_CT     0x00000ff0    /* collision threshold */
 #define E1000_TCTL_COLD   0x003ff000    /* collision distance */
 
+/* Receive Control */
+#define E1000_RCTL_EN             0x00000002    /* enable */
+#define E1000_RCTL_LPE            0x00000020    /* long packet enable */
+#define E1000_RCTL_LBM_NO         0x00000000    /* no loopback mode */
+#define E1000_RCTL_RDMTS_HALF     0x00000000    /* rx desc min threshold size */
+#define E1000_RCTL_RDMTS_QUAT     0x00000100    /* rx desc min threshold size */
+#define E1000_RCTL_RDMTS_EIGTH    0x00000200    /* rx desc min threshold size */
+#define E1000_RCTL_MO_SHIFT       12            /* multicast offset shift */
+#define E1000_RCTL_MO_0           0x00000000    /* multicast offset 11:0 */
+#define E1000_RCTL_MO_1           0x00001000    /* multicast offset 12:1 */
+#define E1000_RCTL_MO_2           0x00002000    /* multicast offset 13:2 */
+#define E1000_RCTL_MO_3           0x00003000    /* multicast offset 15:4 */
+#define E1000_RCTL_BAM            0x00008000    /* broadcast enable */
+/* these buffer sizes are valid if E1000_RCTL_BSEX is 0 */
+#define E1000_RCTL_SZ_2048        0x00000000    /* rx buffer size 2048 */
+#define E1000_RCTL_SZ_1024        0x00010000    /* rx buffer size 1024 */
+#define E1000_RCTL_SZ_512         0x00020000    /* rx buffer size 512 */
+#define E1000_RCTL_SZ_256         0x00030000    /* rx buffer size 256 */
+#define E1000_RCTL_SECRC          0x04000000    /* Strip Ethernet CRC */
+
 #define E1000_STATUS   0x00008  /* Device Status - RO */
+
+/* Interrupt Cause Read */
+#define E1000_ICR_LSC           0x00000004 /* Link Status Change */
+#define E1000_ICR_RXSEQ         0x00000008 /* rx sequence error */
+#define E1000_ICR_RXDMT0        0x00000010 /* rx desc min. threshold (0) */
+#define E1000_ICR_RXO           0x00000040 /* rx overrun */
+#define E1000_ICR_RXT0          0x00000080 /* rx timer intr (ring 0) */
+
+/* Interrupt Mask Set */
+#define E1000_IMS_LSC       E1000_ICR_LSC       /* Link Status Change */
+#define E1000_IMS_RXSEQ     E1000_ICR_RXSEQ     /* rx sequence error */
+#define E1000_IMS_RXDMT0    E1000_ICR_RXDMT0    /* rx desc min. threshold */
+#define E1000_IMS_RXO       E1000_ICR_RXO       /* rx overrun */
+#define E1000_IMS_RXT0      E1000_ICR_RXT0      /* rx timer intr */
 
 int pci_e1000_attach(struct pci_func *pcif);
 int transmit_packet(void *va,uint16_t length);
